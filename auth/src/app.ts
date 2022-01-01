df3431cd29ad5591869046c4e2269f2c10eec9c9
shshingh67 <singhsharandeep676@gmail.com>
2022-01-01 11:29:17 -0800

    app config for auth service

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
+import "express-async-errors";
+
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler, NotFoundError } from "@singhpostitapp/common";
 
 const app = express();
 app.set("trust proxy", true);
@@ -11,5 +17,15 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 export { app };
