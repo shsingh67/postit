0dd4c38455e129bac88241f517a400b133641fef
shshingh67 <singhsharandeep676@gmail.com>
2022-01-02 12:54:39 -0800

    fixed typo in serializeErrors method

--- common/src/middlewares/errorHandler.ts
+++ common/src/middlewares/errorHandler.ts
@@ -8,7 +8,7 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof CustomError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   console.log(err);
