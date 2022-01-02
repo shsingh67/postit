aa40c59d3870e08422c9e222b9d8fbd0954ad2d9
shshingh67 <singhsharandeep676@gmail.com>
2022-01-02 12:50:50 -0800

    added error handling for enviroment variables

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,24 @@
 import { app } from "./app";
+import mongoose from "mongoose";
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+
   try {
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("Connected to MongoDB");
     app.listen(3000, () => {
       console.log("Listening on port 3000!");
     });
-  } catch (err) {}
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 start();
