65178bc4b6b9d4f5450ad0f6d1659f2f1573b72a
shshingh67 <singhsharandeep676@gmail.com>
2022-01-02 13:01:44 -0800

    several test cases for testing the signin route handler

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("sets the cookie when valid credentials are given", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
+
+it("responds with 400 when an invalid email is given", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test20@test.com", password: "password" })
+    .expect(400);
+});
+
+it("responds with 400 when an invalid password is given", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@test.com", password: "adfadfdsf" })
+    .expect(400);
+});
