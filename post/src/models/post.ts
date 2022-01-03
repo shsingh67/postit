1cfcd2b74d3d212c84f98555e0dac175723fa6d5
shshingh67 <singhsharandeep676@gmail.com>
2022-01-03 11:17:57 -0800

    post schema

--- post/src/models/post.ts
+++ post/src/models/post.ts
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
+import { CommentDoc } from "./comment";
 
 interface PostAttrs {
   userId: string;
-  comments?: mongoose.Schema.Types.Array;
-  likes?: mongoose.Schema.Types.Number;
+  caption: string;
+  imgUrl?: string | undefined;
+  comments?: CommentDoc[] | undefined;
+  likes?: number | undefined;
 }
 
 interface PostDoc extends mongoose.Document {
   userId: string;
-  comments: mongoose.Schema.Types.Array;
-  likes: mongoose.Schema.Types.Number;
+  caption: string;
+  imgUrl?: string | undefined;
+  comments?: CommentDoc[] | undefined;
+  likes?: number | undefined;
 }
 
 interface PostModel extends mongoose.Model<PostDoc> {
@@ -22,12 +27,23 @@ const postSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    comments: {
-      type: mongoose.Schema.Types.Array,
+    caption: {
+      type: String,
+      require: true,
+    },
+    imgUrl: {
+      type: String,
       required: false,
     },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
     likes: {
-      type: mongoose.Schema.Types.Number,
+      type: Number,
+      require: false,
     },
   },
   {
@@ -35,10 +51,15 @@ const postSchema = new mongoose.Schema(
       transform(doc, ret) {
         ret.id = ret._id;
         delete ret._id;
+        delete ret.__v;
       },
     },
   }
 );
+
+postSchema.statics.build = (attrs: PostAttrs) => {
+  return new Post(attrs);
+};
 
 const Post = mongoose.model<PostDoc, PostModel>("Post", postSchema);
 
