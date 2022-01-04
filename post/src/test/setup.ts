98bea1c146e74cc458cba3813ef543dfd5d2de42
shshingh67 <singhsharandeep676@gmail.com>
2022-01-04 11:37:24 -0800

    jest setup file

---
+++ post/src/test/setup.ts
@@ -0,0 +1,52 @@
+import jwt from "jsonwebtoken";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+declare global {
+  function signin(): string[];
+}
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = "adfadf";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  //create payload
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+
+  //Build JTW Token
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  //Create session object {jwt: my_jwt}
+  const session = { jwt: token };
+
+  //turn session object into json
+  const sessionJSON = JSON.stringify(session);
+
+  //Encode it to base67
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`session=${base64}`];
+};
