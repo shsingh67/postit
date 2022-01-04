3b07ffaa227b626b93f6b768020e9e8b070c9f6e
shshingh67 <singhsharandeep676@gmail.com>
2022-01-04 11:36:49 -0800

    route allows a post to be edited

---
+++ post/src/routes/update.ts
@@ -0,0 +1,61 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from "@singhpostitapp/common";
+import express, { Request, Response } from "express";
+import { Post } from "../models/post";
+import { body } from "express-validator";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+router.put(
+  "/api/posts",
+  requireAuth,
+  [
+    body("id")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TickedId must be provided"),
+    body("userId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("userId must be provided"),
+    body("caption")
+      .not()
+      .isEmpty()
+      .withMessage("Please provide a caption for your post."),
+    body("imgUrl").optional().not().isEmpty().isURL(),
+    body("likes")
+      .optional()
+      .isInt({ min: 0 })
+      .withMessage(
+        "Number of likes must be a numberic value greater than zero."
+      ),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const post = await Post.findById(req.body.id);
+
+    if (!post) {
+      throw new NotFoundError();
+    }
+
+    if (post.userId != req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    post.set({
+      caption: req.body.caption,
+      imgUrl: req.body.imgUrl,
+      likes: req.body.likes,
+    });
+    res.status(200).send(post);
+  }
+);
+
+export { router as updatePostRouter };
