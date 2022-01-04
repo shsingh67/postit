3d4860836297208b7287ddb9eb3a2673fbe75a63
shshingh67 <singhsharandeep676@gmail.com>
2022-01-04 11:45:52 -0800

    added route handlers

--- post/src/app.ts
+++ post/src/app.ts
@@ -1,8 +1,40 @@
 import express from "express";
 import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import "express-async-errors";
+
+import {
+  currentUser,
+  errorHandler,
+  NotFoundError,
+} from "@singhpostitapp/common";
+import { newPostRouter } from "./routes/new";
+import { updatePostRouter } from "./routes/update";
+import { indexPostRouter } from "./routes";
 
 const app = express();
-
+//Since we are behind ingress-nginx controller
+app.set("trust proxy", true);
 app.use(json());
+//No need to encrypt the cookie since we are using JWT's and only use cookies over a https connection
+//(no need to secure cookies in test env)
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUser);
+
+app.use(newPostRouter);
+app.use(updatePostRouter);
+app.use(indexPostRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 export { app };
