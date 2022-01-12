185cf78fab958401557b5cb881f362d8b2107064
shshingh67 <singhsharandeep676@gmail.com>
2022-01-12 13:01:10 -0800

    updated export statement

--- common/src/index.ts
+++ common/src/index.ts
@@ -15,4 +15,4 @@ export * from "./middlewares/requireAuth";
 export * from "./events/base-listener";
 export * from "./events/base-publisher";
 export * from "./events/comment-created-event";
-export * from "./events/subject";
+export * from "./events/subjects";
