666336377fe73bdc3ec976621ffb7d954a72dafc
shshingh67 <singhsharandeep676@gmail.com>
2022-01-12 19:53:01 -0800

    added logic to initialize comment created listener

--- post/src/index.ts
+++ post/src/index.ts
@@ -1,5 +1,7 @@
 import { app } from "./app";
 import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
+import { CommentCreatedListener } from "./events/listeners/comment-created-listener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -10,7 +12,31 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
 
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL I must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
   try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new CommentCreatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to MongoDB");
     app.listen(3000, () => {
