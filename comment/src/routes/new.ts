1abd07b0c8cee1c451d562ac064f74e67e286d61
shshingh67 <singhsharandeep676@gmail.com>
2022-01-12 18:22:37 -0800

    creates a new comment

---
+++ comment/src/routes/new.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+
+import { Comment } from "../models/comment";
+import { requireAuth, validateRequest } from "@singhpostitapp/common";
+import { CommentCreatedPublisher } from "../events/publishers/comment-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
+
+const router = express.Router();
+
+router.post(
+  "/api/comments",
+  requireAuth,
+  [body("caption").not().isEmpty().withMessage("Please Provide a Comment.")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    //create the comments and save it
+    const { caption, postId } = req.body;
+    const comment = Comment.build({
+      userId: req.currentUser!.id,
+      postId,
+      caption,
+    });
+    await comment.save();
+
+    //publish an event saying a comment was created
+    new CommentCreatedPublisher(natsWrapper.client).publish({
+      id: comment.id!,
+      userId: comment.userId,
+      postId: comment.postId,
+      caption: comment.caption,
+    });
+
+    res.status(201).send(comment);
+  }
+);
+
+export { router as newCommentRouter };
