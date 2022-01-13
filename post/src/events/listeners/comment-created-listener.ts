4c9c5af2b27f4cb88a5bc05c8db27b380e2f44ca
shshingh67 <singhsharandeep676@gmail.com>
2022-01-12 17:58:23 -0800

    listens to new comments being created

---
+++ post/src/events/listeners/comment-created-listener.ts
@@ -0,0 +1,41 @@
+import { Message } from "node-nats-streaming";
+
+import {
+  Listener,
+  CommentCreatedEvent,
+  Subjects,
+} from "@singhpostitapp/common";
+import { queueGroupName } from "./queue-group-name";
+import { Post } from "../../models/post";
+import { Comment, CommentDoc } from "../../models/comment";
+
+export class CommentCreatedListener extends Listener<CommentCreatedEvent> {
+  readonly subject = Subjects.CommentCreated;
+  readonly queueGroupName = queueGroupName;
+
+  async onMessage(data: CommentCreatedEvent["data"], msg: Message) {
+    const { id, userId, postId, caption } = data;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      throw new Error("This post no longer exists.");
+    }
+
+    const comment = Comment.build({ id, userId, postId, caption });
+    await comment.save();
+
+    let comments;
+    if (post.comments === undefined) {
+      comments = [];
+      comments.push(comment);
+      post.set({ comments });
+    } else {
+      post.comments.push(comment);
+    }
+    post.set({ comments });
+    await post.save();
+
+    msg.ack();
+  }
+}
